9c91338068ae639d985a06bf3537ce2323bc2d1a
shay-kennedy <kennedy.rshay@gmail.com>
2016-10-25 16:36:23 -0700

    Updated index.js to import React components and routes

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,9 @@
-import 'babel-polyfill';
+// import 'babel-polyfill';
+var React = require("react");
+import { render } from 'react-dom';
+var routes = require ('./components/router');
+import Main from './components/main';
 
 console.log(`Client running in ${process.env.NODE_ENV} mode`);
+
+render(routes, document.getElementById('app'));
