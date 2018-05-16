db74cd770bda2c1da8bd71ab0471593b0ef14ab6
shay-kennedy <kennedy.rshay@gmail.com>
2018-05-16 01:04:06 -0700

    removing unused import

--- client/js/components/TrailsLanding.js
+++ client/js/components/TrailsLanding.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
-import { Input } from '../components'
 
 
 export class TrailsLanding extends Component {
