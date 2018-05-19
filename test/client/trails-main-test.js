cc830f70de26b8a0b18b3196b29f843444a51924
shay-kennedy <kennedy.rshay@gmail.com>
2018-05-19 10:31:46 -0700

    updated testing

--- test/client/trails-main-test.js
+++ test/client/trails-main-test.js
@@ -3,17 +3,17 @@ import TestUtils from 'react-addons-test-utils'
 import * as chai from 'chai'
 let should = chai.should()
 import { TrailsMain } from '../../client/js/containers'
-import { Provider } from 'react-redux'
+
 
 const renderer = TestUtils.createRenderer()
-renderer.render(<Provider store={}><TrailsMain /></Provider>)
+renderer.render(<TrailsMain.WrappedComponent />)
 const result = renderer.getRenderOutput()
 
 describe('<TrailsMain />', () => {
   it('Renders a div', () => {
     result.type.should.equal('div')
   })
-  // it('Renders two children divs', () => {
-  //   result.props.children.length.should.equal(2)
-  // })
+  it('Renders two children divs', () => {
+    result.props.children.length.should.equal(2)
+  })
 })
